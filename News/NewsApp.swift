a0875c767184b28d4f9f20bf54bf142bb7447cf5
DariaSelezneva <d.selezneva@dunice.net>
2022-05-04 12:23:53 +0300

    Initial Commit

---
+++ News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by dunice on 04.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
